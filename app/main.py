85362e9c0f7b5d020673677f90a398cd383dcf1f
Nico0223 <carlos_dominic_gonzaga@dlsu.edu.ph>
2025-07-09 11:37:43 +0800

    added CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,14 +1,24 @@
 from fastapi import FastAPI
 import os
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 
 # Load environment variables from .env file
 load_dotenv()
 
 from . import scheduler, sentiment, fraud, paymongocheckout
 
-
 app = FastAPI()
+
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Change to your frontend's URL in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(scheduler.router, prefix="/api/scheduler")
 app.include_router(sentiment.router, prefix="/api/sentiment")
